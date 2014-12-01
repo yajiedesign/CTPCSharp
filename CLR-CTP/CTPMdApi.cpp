d16c7218e75f702c845268b2b1c53f0cd7cdfc3f
yajiedesign <yajiedesign@163.com>
2014-12-01 08:40:58 +0800

    价格接收修正

--- CLR-CTP/CTPMdApi.cpp
+++ CLR-CTP/CTPMdApi.cpp
@@ -37,16 +37,24 @@ namespace CTPCLR
 		{
 			FrontAddr = _addr;
 
-			CThostFtdcMdSpi* pUserSpi = new CMdSpi();
+			pUserSpi = new CMdSpi();
 			// 初始化UserApi
 			pUserApi = CThostFtdcMdApi::CreateFtdcMdApi(MarshalString(_FlowPath).c_str());		// 创建UserApi
+
+			EventInit();
 
 			pUserApi->RegisterSpi(pUserSpi);						// 注册事件类
 			pUserApi->RegisterFront((char*)MarshalString(FrontAddr).c_str());					// connect
 			pUserApi->Init();
 		}
 
+		virtual ~MdApiclr(){
 
+			for each (GCHandle var in gchlist)
+			{
+				var.Free();
+			}
+		}
 
 		/// <summary>
 		/// 前置地址
@@ -65,7 +73,7 @@ namespace CTPCLR
 			{
 				for (int i = 0; i < nCount; i++)
 				{
-					IntPtr tp = Marshal::StringToHGlobalAuto(ppInstrumentID[i]);
+					IntPtr tp = Marshal::StringToHGlobalAnsi(ppInstrumentID[i]);
 					ppIntPtrs->Add(tp);
 					ppi[i] = static_cast<char*>(tp.ToPointer());
 				}
@@ -146,5 +154,136 @@ namespace CTPCLR
 				Marshal::FreeHGlobal(bufferIntPtr);
 			}
 		}
+
+		void EventInit()
+		{
+
+			{
+				cppFrontConnect^ fp = gcnew cppFrontConnect(this, &MdApiclr::cppcbFrontConnect);
+				GCHandle gch = GCHandle::Alloc(fp);
+				gchlist->Add(gch);
+				IntPtr ip = Marshal::GetFunctionPointerForDelegate(fp);
+				CBOnFrontConnected cb = static_cast<CBOnFrontConnected>(ip.ToPointer());
+				pUserSpi->cbOnFrontConnected = cb;
+			}
+
+		{
+			cppDisConnected^ fp = gcnew cppDisConnected(this, &MdApiclr::cppcbDisConnected);
+			GCHandle gch = GCHandle::Alloc(fp);
+			gchlist->Add(gch);
+			IntPtr ip = Marshal::GetFunctionPointerForDelegate(fp);
+			CBOnFrontDisconnected cb = static_cast<CBOnFrontDisconnected>(ip.ToPointer());
+			pUserSpi->cbOnFrontDisconnected = cb;
+		}
+		{
+			cppRspUserLogin^ fp = gcnew cppRspUserLogin(this, &MdApiclr::cppcbRspUserLogin);
+			GCHandle gch = GCHandle::Alloc(fp);
+			gchlist->Add(gch);
+			IntPtr ip = Marshal::GetFunctionPointerForDelegate(fp);
+			CBOnRspUserLogin cb = static_cast<CBOnRspUserLogin>(ip.ToPointer());
+			pUserSpi->cbOnRspUserLogin = cb;
+		}
+		{
+			cppRspUserLogout^ fp = gcnew cppRspUserLogout(this, &MdApiclr::cppcbRspUserLogout);
+			GCHandle gch = GCHandle::Alloc(fp);
+			gchlist->Add(gch);
+			IntPtr ip = Marshal::GetFunctionPointerForDelegate(fp);
+			CBOnRspUserLogout cb = static_cast<CBOnRspUserLogout>(ip.ToPointer());
+			pUserSpi->cbOnRspUserLogout = cb;
+		}
+		{
+			cppOnRtnDepthMarketData^ fp = gcnew cppOnRtnDepthMarketData(this, &MdApiclr::cppcbRtnDepthMarketData);
+			GCHandle gch = GCHandle::Alloc(fp);
+			gchlist->Add(gch);
+			IntPtr ip = Marshal::GetFunctionPointerForDelegate(fp);
+			CBOnRtnDepthMarketData cb = static_cast<CBOnRtnDepthMarketData>(ip.ToPointer());
+			pUserSpi->cbOnRtnDepthMarketData = cb;
+		}
+
+		}
+
+
+	public:
+		delegate void FrontConnect();
+	private:
+		delegate void cppFrontConnect();
+		void cppcbFrontConnect()
+		{
+
+			OnFrontConnect();
+		}
+	public:
+		/// <summary>
+		/// 当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用。
+		/// </summary>
+		event FrontConnect ^OnFrontConnect;
+
+
+	public:
+		delegate void DisConnected(int reason);
+	private:
+		delegate void cppDisConnected(int reason);
+		void cppcbDisConnected(int reason)
+		{
+
+			OnDisConnected(reason);
+		}
+	public:
+		/// <summary>
+		/// 当客户端与交易后台通信连接断开时，该方法被调用。当发生这个情况后，API会自动重新连接，客户端可不做处理。
+		/// </summary>
+		event DisConnected ^OnDisConnected;
+
+
+
+	public:
+		delegate void RspUserLogin(CTPCommon::CThostFtdcRspUserLoginField pRspUserLogin, CTPCommon::CThostFtdcRspInfoField pRspInfo, int nRequestID, bool bIsLast);
+	private:
+		delegate void cppRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+		void cppcbRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+		{
+			CTPCommon::CThostFtdcRspUserLoginField  cspRspUserLogin = (CTPCommon::CThostFtdcRspUserLoginField) Marshal::PtrToStructure(IntPtr(pRspUserLogin), CTPCommon::CThostFtdcRspUserLoginField::typeid); CTPCommon::CThostFtdcRspInfoField  cspRspInfo = (CTPCommon::CThostFtdcRspInfoField) Marshal::PtrToStructure(IntPtr(pRspInfo), CTPCommon::CThostFtdcRspInfoField::typeid);
+			OnRspUserLogin(cspRspUserLogin, cspRspInfo, nRequestID, bIsLast);
+		}
+	public:
+		/// <summary>
+		/// 登录请求响应
+		/// </summary>
+		event RspUserLogin ^OnRspUserLogin;
+
+
+	public:
+		delegate void RspUserLogout(CTPCommon::CThostFtdcUserLogoutField pUserLogout, CTPCommon::CThostFtdcRspInfoField pRspInfo, int nRequestID, bool bIsLast);
+	private:
+		delegate void cppRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+		void cppcbRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
+		{
+			CTPCommon::CThostFtdcUserLogoutField  cspUserLogout = (CTPCommon::CThostFtdcUserLogoutField) Marshal::PtrToStructure(IntPtr(pUserLogout), CTPCommon::CThostFtdcUserLogoutField::typeid); CTPCommon::CThostFtdcRspInfoField  cspRspInfo = (CTPCommon::CThostFtdcRspInfoField) Marshal::PtrToStructure(IntPtr(pRspInfo), CTPCommon::CThostFtdcRspInfoField::typeid);
+			OnRspUserLogout(cspUserLogout, cspRspInfo, nRequestID, bIsLast);
+		}
+	public:
+		/// <summary>
+		/// 登出请求响应
+		/// </summary>
+		event RspUserLogout ^OnRspUserLogout;
+
+
+
+
+	public:
+		delegate void RtnDepthMarketData(CTPCommon::CThostFtdcDepthMarketDataField pDepthMarketData);
+	private:
+		delegate void cppOnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData);
+		void cppcbRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData)
+		{
+			CTPCommon::CThostFtdcDepthMarketDataField  cspDepthMarketData = (CTPCommon::CThostFtdcDepthMarketDataField) Marshal::PtrToStructure(IntPtr(pDepthMarketData), CTPCommon::CThostFtdcDepthMarketDataField::typeid);
+			OnRtnDepthMarketData(cspDepthMarketData);
+		}
+	public:
+		/// <summary>
+		/// 期货发起银行资金转期货错误回报
+		/// </summary>
+		event RtnDepthMarketData ^OnRtnDepthMarketData;
+
 	};
 }
